c06b0f279ba611fe157a90a116443eb89b598c98
lucky-dreamer <854461205@qq.com>
2019-12-04 00:36:27 +0800

    Update hello world.py

--- hello world.py
+++ hello world.py
@@ -3,6 +3,6 @@ def main():
   print('I have learned github better')    # 可以建立多个分支，然后添加新版本。其他用于测试，最后将成功的结果合并到master分支上，master分支是主要的可以运行的工作分支
   #如果是团队协作项目的话，添加团队成员，然后发pullrequest讨论审核，再最终合并
 
-  # 如果进行陌生人的开源项目，先fork(拷贝)下来，然后再更改，上传，pullrequest
+  # 如果进行陌生人的开源项目，先fork(拷贝)下来，然后再更改，上传，pullrequest,相当于写一个测试的仓库，需要对方点同意
 if __name__=='__main__':
   main()
